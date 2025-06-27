26f57708c42c37798598794a50ef1f458c06a16d
yousefkhalaf0 <yousefkhalaf667@gmail.com>
2025-06-27 16:17:02 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ProfileTask());
+}
+
+class ProfileTask extends StatelessWidget {
+  const ProfileTask({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: );
+  }
+}
